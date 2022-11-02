b99fbe851c86f840ea936f36d469ef61c0a8b342
Cory Chu <cory@gwlab.page>
2022-11-02 16:05:26 -0500

    clean up src/routes/meetings.js

--- src/routes/meetings.js
+++ src/routes/meetings.js
@@ -13,7 +13,9 @@ router.use(express.json());
 router.post("/:group_nano_id", async (req, res) => {
   // Get Auth0 userID (i.e., sub) from JWT access token.
   const auth0UserID = req.auth.sub;
-  console.log("Create Group by POST api/groups/: " + auth0UserID);
+  console.log(
+    "Create Meeting by POST api/meetings/:group_nanoid : " + auth0UserID
+  );
 
   // Serch for the user in DB
   let db_user = await prisma.user.findUnique({
@@ -31,18 +33,6 @@ router.post("/:group_nano_id", async (req, res) => {
   const user_id = db_user.id;
   // console.log(user_id);
 
-  // const group = await prisma.group.findMany({
-  //   where: {
-  //     nanoId: group_nano_id,
-  //     users: {
-  //       some: {
-  //         userId: user_id,
-  //         OR: [{ role: Role.MANAGER }, { role: Role.MEMBER }],
-  //       }, // Check the User is MANAGER or MEMBER of this group.
-  //     },
-  //   },
-  // });
-
   const group = await prisma.group.findMany({
     where: {
       nanoId: group_nano_id,
@@ -51,6 +41,73 @@ router.post("/:group_nano_id", async (req, res) => {
           userId: user_id,
           role: { hasSome: [Role.MANAGER, Role.MEMBER] },
         }, // Check the User is MANAGER or MEMBER of this group.
+      },
+    },
+  });
+
+  const groupItem = group[0]; // Since we used findMany, we unwrap the returned array.
+
+  if (!groupItem) {
+    res.status(404).send("No such group.");
+    return;
+  }
+
+  const group_id = groupItem.id;
+  const meeting_nanoid = nanoid();
+  const meeting_name = req.body.meeting_name;
+  const meeting = await prisma.meeting.create({
+    data: {
+      nanoId: meeting_nanoid,
+      name: meeting_name,
+      groupId: group_id,
+      users: {
+        create: {
+          user: {
+            connect: { id: user_id },
+          },
+          role: Role.MANAGER,
+        },
+      },
+    },
+    include: {
+      users: true,
+    },
+  });
+
+  res.send(meeting);
+});
+
+// Get Meeting list under Group: group_nanoid
+router.get("/:group_nano_id", async (req, res) => {
+  // Get Auth0 userID (i.e., sub) from JWT access token.
+  const auth0UserID = req.auth.sub;
+  console.log(
+    "Get Meeting list by GET api/meetings/:group_nanoid : " + auth0UserID
+  );
+
+  // Serch for the user in DB
+  let db_user = await prisma.user.findUnique({
+    where: { userIdFromAuth0: auth0UserID },
+  });
+
+  // If the corresponding db_user do not exist, send 403.
+  if (!db_user) {
+    res.status(403).send("Please Update User Profile First.");
+    return;
+  }
+
+  // Get a Group by group_nano_id
+  const group_nano_id = req.params.group_nano_id;
+  const user_id = db_user.id;
+
+  const group = await prisma.group.findMany({
+    where: {
+      nanoId: group_nano_id,
+      users: {
+        some: {
+          userId: user_id,
+          role: { hasSome: [Role.MANAGER, Role.MEMBER] },
+        }, // Check if the User is MANAGER or MEMBER of this group.
       },
     },
   });
