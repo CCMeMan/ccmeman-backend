88de60434b822430307f90891d8f13369d256953
Cory Chu <cory@gwlab.page>
2022-11-05 20:16:38 -0500

    add get meeting

--- src/routes/meetings.js
+++ src/routes/meetings.js
@@ -144,132 +144,60 @@ router.get("/:group_nano_id", async (req, res) => {
   res.status(200).send(meetings);
 });
 
-// // Get a list of Groups of a User
-// router.get("/", async (req, res) => {
-//   // Get Auth0 userID (i.e., sub) from JWT access token.
-//   const auth0UserID = req.auth.sub;
-//   console.log("Get Group by GET api/groups/: " + auth0UserID);
+// Get Meeting
+// GET api/v1/meetings?meeting-nano-id=xxx
+router.get("/", async (req, res) => {
+  const auth0UserID = req.auth.sub;
+  const meetingNanoId = req.query["meeting-nano-id"];
+  console.log(
+    "Get Meeting by GET api/meetings?meetingNanoId=" +
+      meetingNanoId +
+      " by " +
+      auth0UserID
+  );
 
-//   // Serch for the user in DB
-//   let db_user = await prisma.user.findUnique({
-//     where: { userIdFromAuth0: auth0UserID },
-//   });
+  // Serch for the user in DB
+  let db_user = await prisma.user.findUnique({
+    where: { userIdFromAuth0: auth0UserID },
+  });
 
-//   // If the corresponding db_user do not exist, send 403.
-//   if (!db_user) {
-//     res.status(403).send("Please Update User Profile First.");
-//     return;
-//   }
+  if (!db_user) {
+    res.status(403).send("Please Update User Profile First.");
+    return;
+  }
 
-//   const user_id = db_user.id;
-//   console.log(user_id);
-//   // const groups = await prisma.user.findUnique({
-//   //   where: { userIdFromAuth0: auth0UserID },
-//   //   select: {
-//   //     groups: {
-//   //       include: {
-//   //         user: true,
-//   //       },
-//   //     },
-//   //   },
-//   // });
+  const user_id = db_user.id;
 
-//   const groups = await prisma.group.findMany({
-//     where: { users: { some: { userId: user_id } } },
-//     include: {
-//       users: {
-//         include: { user: true },
-//       },
-//     },
-//   });
+  // Find the meeting
+  const meetings = await prisma.meeting.findMany({
+    where: {
+      nanoId: meetingNanoId,
+      users: {
+        some: {
+          userId: user_id,
+          role: { hasSome: [Role.MANAGER, Role.MEMBER, Role.GUEST] },
+        }, // Check if the User is MANAGER or MEMBER or GUEST of this meeting.
+      },
+    },
+    include: {
+      users: {
+        include: {
+          user: {
+            select: { name: true, userIdFromAuth0: true, email: true }, // TODO: maybe to include avatar img url
+          },
+        },
+      },
+      links: true,
+    },
+  });
 
-//   // const group = await prisma.group.create({
-//   //   data: {
-//   //     sUUID: group_suuid,
-//   //     name: group_name,
-//   //     users: {
-//   //       create: {
-//   //         user: {
-//   //           connect: { id: user_id },
-//   //         },
-//   //         role: Role.MANAGER,
-//   //       },
-//   //     },
-//   //   },
-//   //   include: {
-//   //     users: true,
-//   //   },
-//   // });
+  const meeting = meetings[0]; // Since we used findMany, we unwrap the returned array.
+  if (!meeting) {
+    res.status(404).send("No such meeting.");
+    return;
+  }
 
-//   res.send(groups);
-// });
-
-// // Get a Group by group_nano_id
-// router.get("/:group_nano_id", async (req, res) => {
-//   // Get Auth0 userID (i.e., sub) from JWT access token.
-//   const group_nano_id = req.params.group_nano_id;
-//   const auth0UserID = req.auth.sub;
-//   console.log("Get Group by GET api/groups/group_nano_id : " + auth0UserID);
-
-//   // Serch for the user in DB
-//   let db_user = await prisma.user.findUnique({
-//     where: { userIdFromAuth0: auth0UserID },
-//   });
-
-//   // If the corresponding db_user do not exist, send 403.
-//   if (!db_user) {
-//     res.status(403).send("Please Update User Profile First.");
-//     return;
-//   }
-
-//   const user_id = db_user.id;
-//   console.log(user_id);
-
-//   const group = await prisma.group.findMany({
-//     where: {
-//       nanoId: group_nano_id,
-//       users: {
-//         some: { userId: user_id }, // Check the User dose participate in this Group.
-//       },
-//     },
-//     include: {
-//       users: {
-//         include: { user: { select: { name: true } } },
-//       },
-//       childGroups: {
-//         where: {
-//           users: {
-//             some: { userId: user_id }, // ChildGroups that the User participated in
-//           },
-//         },
-//         // include: {
-//         //   users: {
-//         //     include: { user: { select: { name: true } } },
-//         //   },
-//         // },
-//       },
-//       meetings: {
-//         where: {
-//           users: {
-//             some: { userId: user_id }, // Meetings that the User participated in
-//           },
-//         },
-//         // include: {
-//         //   users: {
-//         //     include: { user: { select: { name: true } } },
-//         //   },
-//         // },
-//       },
-//     },
-//   });
-
-//   const groupItem = group[0]; // Since we used findMany, we unwrap the returned array.
-//   if (!groupItem) {
-//     res.status(404).send("No such group.");
-//     return;
-//   }
-
-//   res.send(groupItem);
-// });
+  res.status(200).send(meeting);
+});
 
 export default router;
